db6eb075f63e0ad6e75d334f03b493450e850d89
Rajiv RP <rajiv.rrp@hotmail.com>
2017-04-14 15:20:54 +0530

    Added Detail Action Method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Details()
+        {
+            return "Hello from comic books controller";
+        }
     }
 }
